aecf9805c909f8ab62a59104d4ad2f608af6a735
Sumit <sumit.sth54@gmail.com>
2022-09-28 12:30:34 +0545

    Added dynamic titles for dynamic routes

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -5,15 +5,18 @@ import { useRouter } from 'next/router';
 import { getCategories, getCategoryPost } from '../../services';
 
 import { PostCard, Categories, Loader } from '../../components';
+import Head from 'next/head';
 
-const CategoryPost = ({ posts }) => {
+const CategoryPost = ({ posts, category }) => {
     const router = useRouter();
-
     if(router.isFallback) {
         return <Loader />;
     }
     return (
         <div className="container mx-auto lg:px-10 px-4 mb-8">
+            <Head>
+                <title>{category.name}</title>
+            </Head>
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
                 <div className="col-span-1 lg:col-span-8">
                     {posts.map((post, index) => (
@@ -36,9 +39,14 @@ export default CategoryPost
 
 export async function getStaticProps({ params }) {
     const posts = await getCategoryPost(params.slug);
+    const categories = await getCategories();
+    const category = categories.filter(category=>category.slug===params.slug);
     
     return {
-      props : { posts },
+      props : {
+        posts:posts,
+        category:category[0]
+     },
     }
 }
 
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router';
 import { getPosts, getPostDetails } from '../../services';
 
 import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components';
+import Head from 'next/head';
 
 const PostDetails = ({ post }) => {
     const router = useRouter();
@@ -15,6 +16,9 @@ const PostDetails = ({ post }) => {
 
     return (
         <div className="container mx-auto lg:px-10 px-4 mb-8">
+            <Head>
+                <title>{post.title}</title>
+            </Head>
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
                 <div className="col-span-1 lg:col-span-8">
                     <PostDetail post={post} />
